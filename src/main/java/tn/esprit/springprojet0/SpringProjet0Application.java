877a0905e0cc99f1d3e8feaba251adb529fa66f0
HACINE11 <132098004+HACINE11@users.noreply.github.com>
2024-11-21 16:17:29 +0100

    initialisation du projet

---
+++ src/main/java/tn/esprit/springprojet0/SpringProjet0Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.springprojet0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjet0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjet0Application.class, args);
+	}
+
+}
